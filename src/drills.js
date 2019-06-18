0fe7a05889c9b2e7004032d3842836089628eecc
Robert Wiggins <wigginsro11@gmail.com>
2019-06-18 13:24:57 -0400

    paginate, date after, and total cost calculation methods implemented

--- src/drills.js
+++ src/drills.js
@@ -17,8 +17,8 @@ function getAllTextItems(searchTerm) {
     .where('item_name', 'ILIKE', `%${searchTerm}%`)
     .then(result => console.log(result));
 }
-
-getAllTextItems('chicken');
+/* test exercise 1 by calling function */
+// getAllTextItems('chicken');
 
 
 // exercise 2
@@ -32,3 +32,35 @@ function paginateItems(pageNumber) {
     .then(result => console.log(result));
 }
 
+/* test exercise 2 by calling function */
+// paginateItems(2);
+
+
+// exercise 3 - Get all items added after date
+function addedAfterDate(days) {
+  knexInstance('shopping_list')
+    .select('*')
+    .where('date_added', '<', knexInstance.raw('now() - \'?? days\'::INTERVAL', days))
+    .then(result => console.log(result));
+}
+
+/* test exercise 3 by calling function */
+// REVIEW THIS INTERVAL LOGIC....
+// addedAfterDate(10);
+
+
+// Get the total cost for each category
+function getTotalCost() {
+  knexInstance('shopping_list')
+    .select('category')
+    .count('price as total')
+    .groupBy('category')
+    .then(result => {
+      console.log('COST PER CATEGORY');
+      console.log(result);
+    });
+    
+}
+
+/* test exercise 4 by calling function */
+getTotalCost();
